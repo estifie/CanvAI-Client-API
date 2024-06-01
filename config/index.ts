a97f341e46947a74ed236d32563034edc83de4a9
LntAxe <ertusari0@gmail.com>
2024-06-01 18:43:19 +0300

    feat: Add database connection config

--- config/index.ts
+++ config/index.ts
@@ -3,3 +3,4 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
 export const MODEL_API_URL = process.env.MODEL_API_URL || "http://127.0.0.1:3002";
+export const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING || "";
