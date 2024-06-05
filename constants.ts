29feb986ee325afd16442edfd6a227563a21e1b2
LntAxe <ertusari0@gmail.com>
2024-06-05 11:09:46 +0300

    refactor: Change items

--- constants.ts
+++ constants.ts
@@ -1,1 +1,1 @@
-export const IMAGES = ["airplane", "axe", "bed", "bicycle", "bird", "book", "bucket"];
+export const IMAGES = ["apple", "bicycle", "broom", "campfire", "candle", "car", "chair", "snake", "star", "tree"];
